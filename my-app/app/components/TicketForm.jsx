d1c7dc6cf4d09fb39ffbc91ed9eaf6ae24264999
Prakshal Jain <Prakshal.net12345@gmail.com>
2024-06-09 15:48:41 +0530

    mongo

--- my-app/app/components/TicketForm.jsx
+++ my-app/app/components/TicketForm.jsx
@@ -1,53 +1,92 @@
 "use client"
 
-import React ,{useState} from 'react';
+import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
+
 const TicketForm = () => {
+  const router = useRouter();
 
-const handleChange=(e)=>{
-    const value=e.target.value;
-    const name=e.target.name;
+  const handleChange = (e) => {
+    const value = e.target.value;
+    const name = e.target.name;
 
-    setFormData((prevState)=>({
-        ...prevState,
-        [name]:value,
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
     }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await fetch("/api/Tickets", {
+      method: "POST",
+      body: JSON.stringify({ formData }),
+      "content-type": "application/json",
+    })
+
+    if (!res.ok) {
+      throw new Error("Failed to create Ticket.")
+    }
+    router.refresh();
+    router.push("/");
+  
+  
+  };
+
+
+const startingTicketData = {
+  title: "",
+  description: "",
+  priority: 1,
+  progress: 0,
+  status: "not started",
+  category: "Hardware Problem",
 };
 
-const handleSubmit=()=>{
-    console.log("submitted");
-}
+const [formData, setFormData] = useState(startingTicketData);
+return (
+  <div className='flex justify-center'>
+    <form className='flex flex-col gap-3 w-1/2' method='post' onSubmit={handleSubmit}>
+      <h3>Create your Ticket</h3>
+      <label>Title</label>
+      <input id='title' name='title' type="text" onChange={handleChange} required={true} value={formData.title} />
+      <label>Description</label>
+      <textarea id="description" name="description" onChange={handleChange} required={true} value={formData.description} rows="5" />
 
-    const startingTicketData={
-        title:"",
-        description:"",
-        priority:1,
-        progress:0,
-        status:"not started",
-        category:"Hardware Problem",
-    };
+      <label>category</label>
+      <select name="category" value={formData.category} onChange={handleChange}>
+        <option value="Hardware Problem">Hardware Problem</option>
+        <option value="Hardware Problem">Software Problem</option>
+        <option value="Hardware Problem">Project</option>
+      </select>
 
-    const [formData,setFormData]=useState(startingTicketData);
-  return (
-    <div className='flex justify-center'>
-      <form className='flex flex-col gap-3 w-1/2' method='post' onSubmit={handleSubmit}>
-        <h3>Create your Ticket</h3>
-        <label>Title</label>
-        <input id='title' name='title' type="text" onChange={handleChange} required={true} value={formData.title}/>
-        <label>Description</label>
-        <textarea id="description" name="description" onChange={handleChange} required={true} value={formData.description} rows="5"/>
+      <label>Priority</label>
+      <div>
+        <input id="priority-1" name='priority' type="radio" onChange={handleChange} value={1} checked={formData.priority == 1} />
+        <label>1</label>
+        <input id="priority-2" name='priority' type="radio" onChange={handleChange} value={2} checked={formData.priority == 2} />
+        <label>2</label>
+        <input id="priority-3" name='priority' type="radio" onChange={handleChange} value={3} checked={formData.priority == 3} />
+        <label>3</label>
+        <input id="priority-4" name='priority' type="radio" onChange={handleChange} value={4} checked={formData.priority == 4} />
+        <label>4</label>
+        <input id="priority-5" name='priority' type="radio" onChange={handleChange} value={5} checked={formData.priority == 5} />
+        <label>5</label>
+      </div>
+      <label>Progress</label>
+      <input type="range" id="progress" name="progress" value={formData.progress} min="0" max="100" onChange={handleChange} />
 
-        <label>category</label>
-        <select name="category" value={formData.category} onChange={handleChange}>
-          <option value="Hardware Problem">Hardware Problem</option>
-          <option value="Hardware Problem">Software Problem</option>
-          <option value="Hardware Problem">Project</option>
-        </select>
+      <label>Status</label>
+      <select name="status" value={formData.status} onChange={handleChange}>
+        <option value="not started">Not Started</option>
+        <option value="started">Started</option>
+        <option value="done">Done</option>
+      </select>
 
-
-      </form>
-    </div>
-  )
-}
+      <input type="Submit" className='btn' value="Create Ticket" />
+    </form>
+  </div>
+)
+};
 
 export default TicketForm
